0df73c12fc3d8cb1a0be32a0ecf2788de9723712
Robert C. Alfredson <robert.c.alfredson@gmail.com>
2020-01-12 21:58:53 -0500

    Remove target files

--- target/debug/convert_temp.d
+++
@@ -1,1 +0,0 @@
-/Users/robertcalfredson/code/tempy-rust/target/debug/convert_temp: /Users/robertcalfredson/code/tempy-rust/src/main.rs
--- target/debug/deps/convert_temp-52effa95d7eec764.d
+++
@@ -1,5 +0,0 @@
-/Users/robertcalfredson/code/tempy-rust/target/debug/deps/convert_temp-52effa95d7eec764: src/main.rs
-
-/Users/robertcalfredson/code/tempy-rust/target/debug/deps/convert_temp-52effa95d7eec764.d: src/main.rs
-
-src/main.rs:
